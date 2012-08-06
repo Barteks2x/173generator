3223390e7902f1645f548a65bc108a70b843fcf9
Bartosz Skrzypczak <Bartek_s2@wp.pl>
2012-08-06 10:25:33 +0300

    Update src/org/Barteks2x/b173gen/generator/beta173/MapGenBase.java

--- src/org/Barteks2x/b173gen/generator/beta173/MapGenBase.java
+++ src/org/Barteks2x/b173gen/generator/beta173/MapGenBase.java
@@ -1,16 +1,9 @@
-// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) braces deadcode 
-
 package org.Barteks2x.b173gen.generator.beta173;
 
 import java.util.Random;
 
 import net.minecraft.server.World;
 import net.minecraft.server.IChunkProvider;
-
-// Referenced classes of package net.minecraft.src:
-//            World, IChunkProvider
 
 public class MapGenBase
 {
