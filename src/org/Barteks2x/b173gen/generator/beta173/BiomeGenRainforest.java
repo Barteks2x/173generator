956aa69fd0214b6ff9d467ece51b907724809785
Bartosz Skrzypczak <Bartek_s2@wp.pl>
2012-08-06 10:24:27 +0300

    Update src/org/Barteks2x/b173gen/generator/beta173/BiomeGenRainforest.java

--- src/org/Barteks2x/b173gen/generator/beta173/BiomeGenRainforest.java
+++ src/org/Barteks2x/b173gen/generator/beta173/BiomeGenRainforest.java
@@ -1,15 +1,8 @@
-// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) braces deadcode 
-
 package org.Barteks2x.b173gen.generator.beta173;
 
 import java.util.Random;
 
 import net.minecraft.server.WorldGenerator;
-
-// Referenced classes of package net.minecraft.src:
-//            BiomeGenBase, WorldGenBigTree, WorldGenTrees, WorldGenerator
 
 public class BiomeGenRainforest extends BiomeGenBase
 {
