d71db2d44d5fb61e89b9df7aa0017f872a7d895a
Bartosz Skrzypczak <Bartek_s2@wp.pl>
2012-08-06 10:29:03 +0300

    Update src/org/Barteks2x/b173gen/generator/beta173/WorldGenCactus.java

--- src/org/Barteks2x/b173gen/generator/beta173/WorldGenCactus.java
+++ src/org/Barteks2x/b173gen/generator/beta173/WorldGenCactus.java
@@ -1,7 +1,3 @@
-// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) braces deadcode 
-
 package org.Barteks2x.b173gen.generator.beta173;
 
 import java.util.Random;
@@ -10,9 +6,6 @@ import net.minecraft.server.Block;
 import net.minecraft.server.Material;
 import net.minecraft.server.World;
 import net.minecraft.server.WorldGenerator;
-
-// Referenced classes of package net.minecraft.src:
-//            WorldGenerator, World, Block
 
 public class WorldGenCactus extends WorldGenerator {
     public WorldGenCactus() {
