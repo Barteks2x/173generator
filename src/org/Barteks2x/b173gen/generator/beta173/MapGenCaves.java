a4181b4980c372af8a0f899e806bd19bba478a16
Bartosz Skrzypczak <Bartek_s2@wp.pl>
2012-08-06 10:25:54 +0300

    Update src/org/Barteks2x/b173gen/generator/beta173/MapGenCaves.java

--- src/org/Barteks2x/b173gen/generator/beta173/MapGenCaves.java
+++ src/org/Barteks2x/b173gen/generator/beta173/MapGenCaves.java
@@ -1,17 +1,9 @@
-// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) braces deadcode 
-
 package org.Barteks2x.b173gen.generator.beta173;
 
 import java.util.Random;
 
 import net.minecraft.server.Block;
 import net.minecraft.server.World;
-
-// Referenced classes of package net.minecraft.src:
-//            MapGenBase, MathHelper, Block, BlockGrass, 
-//            World
 
 public class MapGenCaves extends MapGenBase {
 
