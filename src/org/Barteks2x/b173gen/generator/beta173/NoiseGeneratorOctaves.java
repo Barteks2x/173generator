3ad438b32cb686db83ea161b62ac3ec63b21cc62
Bartosz Skrzypczak <Bartek_s2@wp.pl>
2012-08-06 10:27:02 +0300

    Update src/org/Barteks2x/b173gen/generator/beta173/NoiseGeneratorOctaves.java

--- src/org/Barteks2x/b173gen/generator/beta173/NoiseGeneratorOctaves.java
+++ src/org/Barteks2x/b173gen/generator/beta173/NoiseGeneratorOctaves.java
@@ -1,13 +1,6 @@
-// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) braces deadcode 
-
 package org.Barteks2x.b173gen.generator.beta173;
 
 import java.util.Random;
-
-// Referenced classes of package net.minecraft.src:
-//            NoiseGenerator, NoiseGeneratorPerlin
 
 public class NoiseGeneratorOctaves extends NoiseGenerator {
 
