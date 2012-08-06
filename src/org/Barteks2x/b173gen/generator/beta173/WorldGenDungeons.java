ecc99fca9952d20a39764312f1664773e708e418
Bartosz Skrzypczak <Bartek_s2@wp.pl>
2012-08-06 10:30:01 +0300

    Update src/org/Barteks2x/b173gen/generator/beta173/WorldGenDungeons.java

--- src/org/Barteks2x/b173gen/generator/beta173/WorldGenDungeons.java
+++ src/org/Barteks2x/b173gen/generator/beta173/WorldGenDungeons.java
@@ -1,7 +1,3 @@
-// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) braces deadcode 
-
 package org.Barteks2x.b173gen.generator.beta173;
 
 import java.util.Random;
@@ -14,10 +10,6 @@ import net.minecraft.server.TileEntityChest;
 import net.minecraft.server.TileEntityMobSpawner;
 import net.minecraft.server.World;
 import net.minecraft.server.WorldGenerator;
-
-// Referenced classes of package net.minecraft.src:
-//            WorldGenerator, World, Material, Block, 
-//            TileEntityChest, TileEntityMobSpawner, ItemStack, Item
 
 public class WorldGenDungeons extends WorldGenerator
 {
