bddc205fb00da587f3f2a033e0b6e0e377d7dbdd
Bartosz Skrzypczak <Bartek_s2@wp.pl>
2012-08-06 10:29:20 +0300

    Update src/org/Barteks2x/b173gen/generator/beta173/WorldGenClay.java

--- src/org/Barteks2x/b173gen/generator/beta173/WorldGenClay.java
+++ src/org/Barteks2x/b173gen/generator/beta173/WorldGenClay.java
@@ -1,7 +1,3 @@
-// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) braces deadcode 
-
 package org.Barteks2x.b173gen.generator.beta173;
 
 import java.util.Random;
@@ -11,11 +7,6 @@ import net.minecraft.server.Material;
 import net.minecraft.server.MathHelper;
 import net.minecraft.server.World;
 import net.minecraft.server.WorldGenerator;
-
-
-// Referenced classes of package net.minecraft.src:
-//            WorldGenerator, Block, World, Material, 
-//            MathHelper
 
 public class WorldGenClay extends WorldGenerator
 {
