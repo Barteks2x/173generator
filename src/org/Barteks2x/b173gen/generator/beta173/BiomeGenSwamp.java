754815015673c1dac3464284d18d6ee1aac8f48d
Bartosz Skrzypczak <Bartek_s2@wp.pl>
2012-08-06 10:25:01 +0300

    Update src/org/Barteks2x/b173gen/generator/beta173/BiomeGenSwamp.java

--- src/org/Barteks2x/b173gen/generator/beta173/BiomeGenSwamp.java
+++ src/org/Barteks2x/b173gen/generator/beta173/BiomeGenSwamp.java
@@ -1,13 +1,4 @@
-// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) braces deadcode 
-
 package org.Barteks2x.b173gen.generator.beta173;
-
-
-
-// Referenced classes of package net.minecraft.src:
-//            BiomeGenBase
 
 public class BiomeGenSwamp extends BiomeGenBase
 {
