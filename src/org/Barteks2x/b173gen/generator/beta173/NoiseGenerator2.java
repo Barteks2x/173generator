eb5ddd7c192d040caa858624d205152773633172
Bartosz Skrzypczak <Bartek_s2@wp.pl>
2012-08-06 10:26:47 +0300

    Update src/org/Barteks2x/b173gen/generator/beta173/NoiseGenerator2.java

--- src/org/Barteks2x/b173gen/generator/beta173/NoiseGenerator2.java
+++ src/org/Barteks2x/b173gen/generator/beta173/NoiseGenerator2.java
@@ -1,7 +1,3 @@
-// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) braces deadcode 
-
 package org.Barteks2x.b173gen.generator.beta173;
 
 import java.util.Random;
