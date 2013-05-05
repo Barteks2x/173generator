edd90ee2a5c2ecaf90002cf5405def9fb03c84df
bartek <bartek@bartek-P31-DS3L>
2013-05-05 20:23:53 +0200

    Added junit, added src/main/

--- src/main/java/org/Barteks2x/b173gen/generator/ChunkProviderGenerate.java
+++ src/main/java/org/Barteks2x/b173gen/generator/ChunkProviderGenerate.java
@@ -1,6 +1,3 @@
-// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) braces deadcode 
 package org.Barteks2x.b173gen.generator;
 
 import java.util.ArrayList;
